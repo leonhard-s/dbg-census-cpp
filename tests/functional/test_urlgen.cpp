012a0e67e91688957ff632b6ba004ab9fe1493ab
Leonhard S <leonhard-sei@outlook.com>
2023-08-18 22:23:52 +0200

    Add functional tests for URL generator

---
+++ tests/functional/test_urlgen.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "dbg_census/urlgen/url_builder.h"
+
+TEST(ExampleQueries, GetEverquest2Collections) {
+    dbg_census::urlgen::UrlBuilder builder("eq2", "", "s:example");
+    auto url = builder.build();
+    EXPECT_EQ(url, "https://census.daybreakgames.com/s:example/get/eq2");
+}
+
+TEST(ExampleQueries, GetPs2CharacterByName) {
+    dbg_census::urlgen::UrlBuilder builder("ps2", "character", "s:example");
+    builder.addQueryTerm("name.first_lower", "auroram");
+    auto url = builder.build();
+    EXPECT_EQ(url, "https://census.daybreakgames.com/s:example/get/ps2/character?name.first_lower=auroram");
+}
+
+TEST(ExampleQueries, GetFacilityTypesFromSanctuary) {
+    dbg_census::urlgen::UrlBuilder builder("ps2", "facility_type", "s:example");
+    builder.setBaseUrl("https://census.lithafalcon.cc");
+    builder.addQueryCommand("c:limit=100");
+    auto url = builder.build();
+    EXPECT_EQ(url, "https://census.lithafalcon.cc/s:example/get/ps2/facility_type?c:limit=100");
+}
