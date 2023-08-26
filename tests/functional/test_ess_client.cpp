bc5697b19144c3293defaae59db5d8f74118ed9d
Leonhard S <leonhard-sei@outlook.com>
2023-08-26 13:01:22 +0200

    Add functional test for ESS connection
    Resolves #10

---
+++ tests/functional/test_ess_client.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include "dbg_census/stream/ess_client.h"
+
+
+TEST(EssClient, ConnectToDefaultEndpoint) {
+    dbg_census::stream::EssClient client;
+    bool client_ready = false;
+    client.setOnReadyCallback([&client_ready]() { client_ready = true; });
+
+    using namespace std::chrono_literals;
+    const std::chrono::milliseconds timeout{ 5s };
+
+    const auto start = std::chrono::steady_clock::now();
+    client.connect();
+    while(std::chrono::steady_clock::now() < start + timeout) {
+        std::this_thread::sleep_for(100ms);
+        if (client_ready) {
+            break;
+        }
+    }
+    if(!client_ready) {
+        FAIL() << "Client did not connect within " << timeout.count() << "ms";
+    }
+    client.disconnect();
+}
